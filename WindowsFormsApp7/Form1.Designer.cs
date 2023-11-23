f881b18c2c60e12d5bbbe1bada423c6f944acdec
K0rin <34002181+K0rin@users.noreply.github.com>
2023-11-23 19:04:26 +0200

    Add project files.

---
+++ WindowsFormsApp7/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Start = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::WindowsFormsApp7.Properties.Resources.paper;
+            this.pictureBox6.Location = new System.Drawing.Point(622, 264);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(95, 85);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::WindowsFormsApp7.Properties.Resources.scissors;
+            this.pictureBox5.Location = new System.Drawing.Point(622, 144);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(95, 85);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::WindowsFormsApp7.Properties.Resources.rock;
+            this.pictureBox4.Location = new System.Drawing.Point(622, 25);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(95, 85);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::WindowsFormsApp7.Properties.Resources.paper;
+            this.pictureBox3.Location = new System.Drawing.Point(84, 264);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(95, 85);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WindowsFormsApp7.Properties.Resources.scissors;
+            this.pictureBox2.Location = new System.Drawing.Point(84, 144);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(95, 85);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp7.Properties.Resources.rock;
+            this.pictureBox1.Location = new System.Drawing.Point(84, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(95, 85);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(148, 385);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(527, 30);
+            this.label1.TabIndex = 6;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(369, 176);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 31);
+            this.Start.TabIndex = 7;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 476);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Start;
+    }
+}
+
---
+++ WindowsFormsApp7/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        int playerChoise = 0;
+        int computerChoise = 0;
+        int statusGame = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int generateChoise() 
+        {
+            Random ran = new Random();
+            int genericInt = ran.Next(1,4);
+            return genericInt;
+        }
+
+        private string selectWinner() 
+        {
+            string text = "";
+            if (playerChoise == 1 && computerChoise == 2) 
+            {
+                picturePaint();
+                text = "You won a game";
+                computerChoise = 0;
+            }
+            if (playerChoise == 1 && computerChoise == 3)
+            {
+                picturePaint();
+                text = "You loose";
+                computerChoise = 0;
+            }
+            if (playerChoise == 2 && computerChoise == 1)
+            {
+                picturePaint();
+                text = "You loose";
+                computerChoise = 0;
+            }
+            if (playerChoise == 2 && computerChoise == 3)
+            {
+                picturePaint();
+                text = "You won a game";
+                computerChoise = 0;
+            }
+            if (playerChoise == 3 && computerChoise == 1)
+            {
+                picturePaint();
+                text = "You won a game";
+                computerChoise = 0;
+            }
+            if (playerChoise == 3 && computerChoise == 2)
+            {
+                picturePaint();
+                text = "You loose";
+                computerChoise = 0;
+            }
+            if (playerChoise == 1 && computerChoise == 1)
+            {
+                picturePaint();
+                text = "A draw. Press start button for a new round";
+                computerChoise = 0;
+            }
+            if (playerChoise == 2 && computerChoise == 2)
+            {
+                picturePaint();
+                text = "A draw. Press start button for a new round";
+                computerChoise = 0;
+            }
+            if (playerChoise == 3 && computerChoise == 3)
+            {
+                picturePaint();
+                text = "A draw. Press start button for a new round";
+                computerChoise = 0;
+            }
+            return text;
+        }
+
+        private void picturePaint()
+        {
+            if (computerChoise == 1)
+            {
+                using (Graphics g = this.CreateGraphics())
+                {
+                    g.DrawRectangle(new Pen(Color.Red, 6), pictureBox4.Bounds);
+                }
+            }
+            else if (computerChoise == 2)
+            {
+                using (Graphics g = this.CreateGraphics())
+                {
+                    g.DrawRectangle(new Pen(Color.Red, 6), pictureBox5.Bounds);
+                }
+            }
+            else if (computerChoise == 3) 
+            {
+                using (Graphics g = this.CreateGraphics())
+                {
+                    g.DrawRectangle(new Pen(Color.Red, 6), pictureBox6.Bounds);
+                }
+            }
+            
+        }
+
+        private void clearComputerChoise() 
+        {
+            using (Graphics g = this.CreateGraphics())
+            {
+                g.DrawRectangle(new Pen(Color.White, 6), pictureBox4.Bounds);
+            }
+            using (Graphics g = this.CreateGraphics())
+            {
+                g.DrawRectangle(new Pen(Color.White, 6), pictureBox5.Bounds);
+            }
+            using (Graphics g = this.CreateGraphics())
+            {
+                g.DrawRectangle(new Pen(Color.White, 6), pictureBox6.Bounds);
+            }
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            clearComputerChoise();
+            computerChoise = generateChoise();
+            statusGame = 1;
+            label1.Text = computerChoise.ToString();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (statusGame < 1) {
+                MessageBox.Show("Please click 'start' button to begin a game");
+                return;
+            }
+            playerChoise = 1;
+            label1.Text = selectWinner();
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (statusGame < 1)
+            {
+                MessageBox.Show("Please click 'start' button to begin a game");
+                return;
+            }
+            playerChoise = 2;
+            label1.Text = selectWinner();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            if (statusGame < 1)
+            {
+                MessageBox.Show("Please click 'start' button to begin a game");
+                return;
+            }
+            playerChoise = 3;
+            label1.Text = selectWinner();
+        }
+    }
+}
